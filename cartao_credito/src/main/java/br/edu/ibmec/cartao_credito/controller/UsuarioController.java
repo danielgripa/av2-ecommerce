7d109d179f70764ad2699db2fd8c7fa350707415
RAFAEL BENTO CRUZ <09072429796@professores.ibmec.edu.br>
2024-11-05 20:09:55 -0300

    Criando a Api de transacao

--- cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/controller/UsuarioController.java
+++ cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/controller/UsuarioController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ibmec.cartao_credito.model.Cartao;
 import br.edu.ibmec.cartao_credito.model.Usuario;
 import br.edu.ibmec.cartao_credito.service.UsuarioService;
 import jakarta.validation.Valid;
@@ -25,5 +27,12 @@ public class UsuarioController {
         usuarioService.criarUsuario(usuario);
         return new ResponseEntity<>(usuario, HttpStatus.CREATED);
     }
-    
+
+    @PostMapping("{id}")
+    public ResponseEntity<Usuario> associarCartao(@PathVariable("id") int id, @RequestBody Cartao cartao) throws Exception { 
+        usuarioService.associarCartao(cartao, id);
+        Usuario usuario = usuarioService.buscaUsuario(id);
+        return new ResponseEntity<>(usuario, HttpStatus.CREATED);
+    }
+   
 }
