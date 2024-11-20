a4d6af87e5b408cf9f5644701c765383238c7964
danielgripa <daniel.gripa@gmail.com>
2024-11-20 13:34:28 -0300

    del: removido pasta cartao_credito

--- cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/exception/UsuarioException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ibmec.cartao_credito.exception;
-
-public class UsuarioException extends Exception {
-    public UsuarioException(String message) {
-        super(message);
-    }
-}
--- cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/exception/ValidationError.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ibmec.cartao_credito.exception;
-
-import lombok.Data;
-
-@Data
-public class ValidationError {
-    private String field;
-    private String message;
-}
